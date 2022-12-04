dc73d8dd8da8bb2d1ece84a52a2650506f020e97
Arrinal S <arrinalsf@gmail.com>
2022-12-04 19:14:32 +0700

    Initial Commit

---
+++ Lasportago/LasportagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LasportagoApp.swift
+//  Lasportago
+//
+//  Created by Arrinal Sholifadliq on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LasportagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
